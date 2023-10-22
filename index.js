40c14b22a16a59c3de4b72c27c6b40041d884b4b
ToryK96 <136476974+ToryK96@users.noreply.github.com>
2023-10-22 09:23:59 +0100

    convert to f and c made global
    previously convert to f and c would only work with current location, now works with searched location

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function currentPosition(position) {
     document.querySelector(
       ".wind-stats-now"
     ).innerHTML = `${windSpeedNow}mph wind`;
+    celsiusTemperature = response.data.daily[0].temperature.day;
   }
 
   axios.get(apiLocal).then(getWeather);
@@ -88,17 +89,19 @@ function displayCity(event) {
   function searchWeather(response) {
     console.log(response);
     console.log(response.data.temperature.current);
-    let currentTemp = Math.round(response.data.temperature.current);
+    let currentTemp = document.querySelector("#current-temp");
     let weatherType = response.data.condition.description;
     let windSpeedNow = Math.round(response.data.wind.speed * 2.23694);
     let iconElementNow = document.querySelector("#weather-icon-now");
     iconElementNow.setAttribute("src", response.data.condition.icon_url);
     iconElementNow.setAttribute("alt", response.data.condition.description);
-    document.querySelector("#current-temp").innerHTML = `${currentTemp}℃`;
+    currentTemp.innerHTML = Math.round(response.data.temperature.current);
+    document.querySelector("#units").innerHTML = "℃";
     document.querySelector("#cloud-cover").innerHTML = `${weatherType}`;
     document.querySelector(
       ".wind-stats-now"
     ).innerHTML = `${windSpeedNow}mph wind`;
+    celsiusTemperature = response.data.temperature.current;
   }
   axios.get(apiSearch).then(searchWeather);
 }
@@ -112,19 +115,23 @@ function getLocalWeather(event) {
 }
 currentBTN.addEventListener("click", getLocalWeather);
 
-//covert to f
+//convert to f
 function farConvert(event) {
   event.preventDefault();
-  let displayedLocation = document.querySelector("#currentLoc");
-  let apiKey = "7tc34ea233a0fd494dc646afo0f7bac1";
-  let apiSearch = `https://api.shecodes.io/weather/v1/current?query=${displayedLocation}&key=${apiKey}&units=imperial`;
-  function convertF(response) {
-    console.log(response);
-    console.log(response.data.temperature.current);
-    let currentTemp = Math.round(response.data.temperature.current);
-    document.querySelector("#current-temp").innerHTML = `${currentTemp}℉`;
-  }
-  axios.get(apiSearch).then(convertF);
+  let temperatureElement = document.querySelector("#current-temp");
+  let farConvert = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(farConvert);
+  document.querySelector("#units").innerHTML = "°F";
 }
 let displayFar = document.querySelector("#convert-far");
 displayFar.addEventListener("click", farConvert);
+
+//convert to c
+function celsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  document.querySelector("#units").innerHTML = "℃";
+}
+let displayCel = document.querySelector("#convert-cel");
+displayCel.addEventListener("click", celsius);
